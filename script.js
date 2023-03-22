66e7d694d2343368281f5ba0b15e772958139c17
InjuMichorius <inju@hotmail.nl>
2023-03-22 21:26:57 +0100

    Arrow scroll effect

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-window.onscroll = function() {
-    const theta = document.documentElement.scrollTop / 50 % Math.PI;
-    const test = document.documentElement.scrollTop - 800
+const body = document.querySelector('body');
 
-document.getElementById('js-logo').style.transform ='rotate(' + window.pageYOffset + "deg)';
-console.log(theta)
-}
+body.onscroll = function () {
+    const theta = document.documentElement.scrollTop / 50 % Math.PI;
+    const test = document.documentElement.scrollTop - 800;
+
+    document.getElementById('js-logo').style.paddingLeft = 200 + test/4 + 'px';
+
+    // document.getElementById('js-logo').style.transform = 'rotate(' + theta + 10000 + 'rad)';
+    console.log(test);
+};
+
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show');
+//         } else {
+//             entry.target.classList.remove('show');
+//         }
+//     })
+// })
+
+// const rotateElement = document.querySelectorAll('.hidden');
+// rotateElement.forEach((el) => observer.observe(el));
+
+// window.onscroll = function () {
+//     scrollRotate();
+// };
