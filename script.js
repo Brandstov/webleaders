7edb368cfbd368d9d263daa3c44f0781c7848961
InjuMichorius <inju@hotmail.nl>
2023-03-22 22:40:02 +0100

    Parallax effect

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const body = document.querySelector('body');
 
 body.onscroll = function () {
     const theta = document.documentElement.scrollTop / 50 % Math.PI;
-    const test = document.documentElement.scrollTop - 800;
+    const topDistance = document.documentElement.scrollTop - 800;
 
-    document.getElementById('js-logo').style.paddingLeft = 200 + test/4 + 'px';
+    document.getElementById('js-logo').style.paddingLeft = 200 + topDistance/4 + 'px';
 
     // document.getElementById('js-logo').style.transform = 'rotate(' + theta + 10000 + 'rad)';
     console.log(test);
 };
+
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = '-' + offset / 8 + "px";
+    // DIV 1 background will move slower than other elements on scroll.
+  });
 
 // const observer = new IntersectionObserver((entries) => {
 //     entries.forEach((entry) => {
